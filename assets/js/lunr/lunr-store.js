3e0c9029a565bdb98ccb42c51285c65a93b21b4c
xyztemat <iamdziub@yahoo.com>
2024-12-02 15:01:07 +0100

    pierwsza zmiana

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Ranking kont bankowych - Grudzień 2024 [ZAKTUALIZOWANO]",
+        "excerpt":"Witamy w naszym najnowszym rankingu kont bankowych na grudzień 2024 roku! 🌟 W dzisiejszych czasach wybór odpowiedniego konta bankowego jest kluczowy dla zarządzania finansami osobistymi i firmowymi. Dlatego przygotowaliśmy dla Ciebie zaktualizowane zestawienie najlepszych ofert na rynku, które pomoże Ci podjąć świadomą decyzję. W naszym rankingu uwzględniliśmy najważniejsze kryteria, takie...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2024/12/01/welcome-to-jekyll.html",
+        "teaser": null
+      }]
